4182b12ed9747a6483bf0eb94998736221ec0ca2
Gabriel Korherr <gabrielkorherr@gmail.com>
2024-07-01 14:36:31 +0200

    Bit of error handling

--- src/main.c
+++ src/main.c
@@ -2,10 +2,14 @@
 #include <gtk/gtk.h>
 
 static void apply(GtkApplication *app, gpointer data) {
-    (void) data;
+    char *title = (char*) data;
     printf("Starting application\n");
     GtkWidget *window = gtk_application_window_new(app);
-    gtk_window_set_title(GTK_WINDOW(window), "Bezier");
+    if (!window) {
+        fprintf(stderr, "Window could not be created\n");
+        return;
+    }
+    gtk_window_set_title(GTK_WINDOW(window), title);
     const int WINDOW_WIDTH = 500;
     const int WINDOW_HEIGHT = 300;
     gtk_window_set_default_size(GTK_WINDOW(window), WINDOW_WIDTH, WINDOW_HEIGHT);
@@ -19,7 +23,7 @@ int main(int argc, char **argv) {
         fprintf(stderr, "Application could not be created\n");
         return 1;
     }
-    g_signal_connect(app, "activate", G_CALLBACK(apply), NULL);
+    g_signal_connect(app, "activate", G_CALLBACK(apply), argc > 1 ? argv[1] : "Default title");
     int exit_status = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref(app);
     return exit_status;
